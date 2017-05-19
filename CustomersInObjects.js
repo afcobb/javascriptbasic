5c5db1625f78e6bac5a6cc0645eef7a19a766dc2
Alex <afcobb@gmail.com>
2017-05-19 11:18:48 -0400

    customersinobjects

---
+++ CustomersInObjects.js
@@ -0,0 +1,29 @@
+var customers = [
+	{ id: 1000, 
+		name: "Waldorf Electronics",
+		address: "123 Any Street",
+		city: "Berlin",
+		state: "Germany",
+		postalcoade: "ABC123"
+		// this is the data for the first object entry, note no comma after final attribute and curly brackets are at the end
+	},
+	{id: 1010, 
+		name: "Google",
+		address: "1 Google Way",
+		city: "Mountain View",
+		state: "California",
+		postalcoade: "12345"
+	},
+	{id: 1020, 
+		name: "Native Instruments",
+		address: "2 Guitar Way",
+		city: "New York",
+		state: "New York",
+		postalcoade: "10101"
+	}
+]
+var customers = [
+];
+function listCustomers() {
+	console.log("Called listCustomers()");
+}
