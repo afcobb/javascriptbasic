90a941b2cf860f7ee2ea9cdcefa832f5949fd6e6
Alex <afcobb@gmail.com>
2017-05-19 12:07:04 -0400

    table date

--- CustomersInObjects.js
+++ CustomersInObjects.js
@@ -4,7 +4,7 @@ var customers = [
 		address: "123 Any Street",
 		city: "Berlin",
 		state: "Germany",
-		postalcoade: "ABC123"
+		postalcode: "ABC123"
 		// this is the data for the first object entry, note no comma after final attribute and curly brackets are at the end
 	},
 	{id: 1010, 
@@ -12,18 +12,64 @@ var customers = [
 		address: "1 Google Way",
 		city: "Mountain View",
 		state: "California",
-		postalcoade: "12345"
+		postalcode: "12345"
 	},
 	{id: 1020, 
 		name: "Native Instruments",
 		address: "2 Guitar Way",
 		city: "New York",
 		state: "New York",
-		postalcoade: "10101"
+		postalcode: "10101"
 	}
-]
-var customers = [
 ];
+
+function formatTableRow(id, name, address, postal) {
+	var tbodyCtrl = document.getElementById("customers")
+	// this  creates a variable to work with the tbody tag. a reference so we can add new elements
+	var trCtrl = document.createElement("tr");
+		// creates table row element. creates opening and closing tags automatically.
+	tbodyCtrl.appendChild(trCtrl)
+		// this tells us to put the tr tag inside the tbody tag, make it a child of tbody - DO THIS FOR EACH FIELD
+	var tdIdCtrl = document.createElement("td");
+		// creates the tag for td element
+	trCtrl.appendChild(tdIdCtrl);
+		// this adds the tdIdCtrl tag to the row. 
+	var textIdCtrl = document.createTextNode(id);
+	tdIdCtrl.appendChild(textIdCtrl);
+	// NOW FOR NAME
+	var tdNameCtrl = document.createElement("td");
+	trCtrl.appendChild(tdNameCtrl);
+	var textNameCtrl = document.createTextNode(name);
+	tdNameCtrl.appendChild(textNameCtrl);
+	// NOW FOR ADDRESS
+	var tdaddressCtrl = document.createElement("td");
+	trCtrl.appendChild(tdaddressCtrl);
+	var textaddressCtrl = document.createTextNode(address);
+	tdaddressCtrl.appendChild(textaddressCtrl);
+	//
+	var tdPostalCtrl = document.createElement("td");
+	trCtrl.appendChild(tdPostalCtrl);
+	var textPostalCtrl = document.createTextNode(postal);
+	tdPostalCtrl.appendChild(textPostalCtrl);
+
+
+}
 function listCustomers() {
 	console.log("Called listCustomers()");
+	for(var idx= 0; idx < customers.length; idx++) {
+		var id = customers[idx].id;
+			// this gives us the id for a specific customer 
+		var name = customers[idx].name;
+		var address = customers[idx].address;
+		var citystpostal = customers[idx].city + ", " +
+								customers[idx].state + " " +
+								customers[idx].postalcode;
+								console.log(id, name, address, citystpostal);	
+								// the formatting for citystpostal has 3 components, so we've added them all together
+		formatTableRow(id, name, address, citystpostal)
+		
+
+
+		} // end for loop
+
 }
